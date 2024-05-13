d8a250739d391487204377424817c8d3e6223e7b
Miha2002 <iancu_miha_flori@yahoo.com>
2024-05-13 17:50:25 +0300

    Minor changes

--- og_main.py
+++ og_main.py
@@ -6,11 +6,8 @@ from PIL import Image
 # Configure
 path_img = "cal.png"
 
-# Stores the result (points of convex hull)
 hull = set()
 
-# Returns the side of point p with respect to line
-# joining points p1 and p2.
 def findSide(p1, p2, p):
     val = (p[1] - p1[1]) * (p2[0] - p1[0]) - (p2[1] - p1[1]) * (p[0] - p1[0])
 
@@ -20,15 +17,12 @@ def findSide(p1, p2, p):
         return -1
     return 0
 
-# returns a value proportional to the distance
-# between the point p and the line joining the
-# points p1 and p2
+
 def lineDist(p1, p2, p):
     return abs((p[1] - p1[1]) * (p2[0] - p1[0]) -
                (p2[1] - p1[1]) * (p[0] - p1[0]))
 
-# End points of line L are p1 and p2. side can have value
-# 1 or -1 specifying each of the parts made by the line L
+
 def quickHull(a, n, p1, p2, side):
     ind = -1
     max_dist = 0
@@ -53,14 +47,13 @@ def quickHull(a, n, p1, p2, side):
     quickHull(a, n, a[ind], p1, -findSide(a[ind], p1, p2))
     quickHull(a, n, a[ind], p2, -findSide(a[ind], p2, p1))
 
+
 def printHull(a, n):
     # a[i].second -> y-coordinate of the ith point
     if (n < 3):
         print("Convex hull not possible")
         return
 
-    # Finding the point with minimum and
-    # maximum x-coordinate
     min_x = 0
     max_x = 0
     for i in range(1, n):
@@ -69,39 +62,29 @@ def printHull(a, n):
         if a[i][0] > a[max_x][0]:
             max_x = i
 
-    # Recursively find convex hull points on
-    # one side of line joining a[min_x] and
-    # a[max_x]
     quickHull(a, n, a[min_x], a[max_x], 1)
-
-    # Recursively find convex hull points on
-    # other side of line joining a[min_x] and
-    # a[max_x]
     quickHull(a, n, a[min_x], a[max_x], -1)
 
     return hull
 
+
 # Driver code
 start_time = time.time()
 
-# Load the black and white image
+
 img = Image.open(path_img).convert("L")
 width, height = img.size
 pixels = img.load()
 
-# Extract black pixel coordinates
 black_pixels = []
 for y in range(height):
     for x in range(width):
         if pixels[x, y] == 0:  # Assuming black pixel value is 0
             black_pixels.append((x, y))
-
 n = len(black_pixels)
-
-# Execute printHull function
 all_points = printHull(black_pixels, n)
 
-# Calculate centroid
+
 def calculate_centroid(points):
     x = [point[0] for point in points]
     y = [point[1] for point in points]
@@ -109,18 +92,17 @@ def calculate_centroid(points):
     centroid_y = sum(y) / len(points)
     return centroid_x, centroid_y
 
-# Calculate angle
+
 def calculate_angle(point, centroid):
     return np.arctan2(point[1] - centroid[1], point[0] - centroid[0])
+
 
 # Measure execution time
 end_time = time.time()
 print("\nExecution time: {:.4f} seconds".format(end_time - start_time))
 
-# Sort points by angle
 centroid = calculate_centroid(all_points)
 sorted_points = sorted(all_points, key=lambda p: calculate_angle(p, centroid))
-
 og_image = cv.imread(path_img, cv.IMREAD_GRAYSCALE)
 output_image = cv.cvtColor(og_image, cv.COLOR_GRAY2BGR)
 cv.polylines(output_image, [np.array(sorted_points)], isClosed=True, color=(0, 255, 0), thickness=2)
